7d98d7ef483141a50669e7ee474f5c511de92221
Matt Maree <dj_matt_maree@yahoo.com>
2018-10-03 13:06:58 +0200

    Added google maps to registration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,9 +25,22 @@ export class AppComponent implements OnInit, AfterViewInit {
 
     filteredBrands: any[];
 
-    brand: string;  
+    brand: string;
+
+    editDate: boolean;
+
+    iconDate: String;
+
+    labelDate: String;
+
+    date: Date;
+
+    options: any[];
+
+    overlays: any[];
+
+    maps: boolean;
     
-  
   @ViewChild('bigMenu') bigMenu : Menu;
   @ViewChild('smallMenu') smallMenu : Menu;
 
@@ -38,8 +51,8 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.loginForm = this.fb.group({
-      names: ['', [Validators.required, Validators.minLength(3)]],      
-      phoneNumber: ['', [Validators.required, Validators.minLength(10)]],
+      names: ['', [Validators.required, Validators.minLength(3)]],
+      phoneNumber: ['', [Validators.required, Validators.maxLength(10)]],
       emailAddress: ['', [Validators.required, Validators.minLength(10)]],
       password: ['', [Validators.required, Validators.minLength(10)]],
       
@@ -50,7 +63,7 @@ export class AppComponent implements OnInit, AfterViewInit {
       regPhoneNumber: ['', [Validators.required, Validators.maxLength(10)]],
       regEmailAddress: ['', [Validators.required, Validators.minLength(10)]],
       regResAddress: ['', [Validators.required, Validators.minLength(10)]],
-      regBillAddress: ['', [Validators.required, Validators.minLength(10)]],
+      regBirthDate: ['', [Validators.required, Validators.minLength(10)]],
       regPassword: ['', [Validators.required, Validators.minLength(10)]],
       regConfirm: ['', [Validators.required, Validators.minLength(10)]]
         })
@@ -59,10 +72,10 @@ export class AppComponent implements OnInit, AfterViewInit {
       supSurname: ['', [Validators.required, Validators.minLength(3)]],
       supPhoneNumber: ['', [Validators.required, Validators.maxLength(10)]],
       supEmailAddress: ['', [Validators.required, Validators.minLength(10)]],
-      regResAddress: ['', [Validators.required, Validators.minLength(10)]],
-      regBillAddress: ['', [Validators.required, Validators.minLength(10)]],     
+      supResAddress: ['', [Validators.required, Validators.minLength(10)]],
+      supBirthDate: ['', [Validators.required, Validators.minLength(10)]],     
       supPassword: ['', [Validators.required, Validators.minLength(10)]],
-      supConfirm:     [Validators.required, Validators.minLength(10)]]
+      supConfirm: ['', [Validators.required, Validators.minLength(10)]],
     })
     let handleSelected = function(event) {
       let allMenus = jQuery(event.originalEvent.target).closest('ul');
@@ -91,7 +104,29 @@ export class AppComponent implements OnInit, AfterViewInit {
       let miniItem = { icon: item.icon, routerLink: item.routerLink }
       this.miniMenuItems.push(miniItem);
     })
+    this.iconDate = 'fa fa-edit';
+    this.labelDate = 'Edit';
+    this.editDate = false;
+    this.maps = false;
+    this.options = {
+      center: {lat: 36.890257, lng: 30.707417},
+      zoom: 12
+  };
 
+  this.overlays = [
+      new google.maps.Marker({position: {lat: 36.879466, lng: 30.667648}, title:"Konyaalti"}),
+      new google.maps.Marker({position: {lat: 36.883707, lng: 30.689216}, title:"Ataturk Park"}),
+      new google.maps.Marker({position: {lat: 36.885233, lng: 30.702323}, title:"Oldtown"}),
+      new google.maps.Polygon({paths: [
+          {lat: 36.9177, lng: 30.7854},{lat: 36.8851, lng: 30.7802},{lat: 36.8829, lng: 30.8111},{lat: 36.9177, lng: 30.8159}
+      ], strokeOpacity: 0.5, strokeWeight: 1,fillColor: '#1976D2', fillOpacity: 0.35
+      }),
+      new google.maps.Circle({center: {lat: 36.90707, lng: 30.56533}, fillColor: '#1976D2', fillOpacity: 0.35, strokeWeight: 1, radius: 1500}),
+      new google.maps.Polyline({path: [{lat: 36.86149, lng: 30.63743},{lat: 36.86341, lng: 30.72463}], geodesic: true, strokeColor: '#FF0000', strokeOpacity: 0.5, strokeWeight: 2})
+  ];
+  }
+  showMaps() {
+    this.maps = true;
   }
   filterBrands(event) {
     this.filteredBrands = [];
@@ -102,9 +137,18 @@ export class AppComponent implements OnInit, AfterViewInit {
           }
     }
 }
+editDates() {
+  if (this.editDate) {
+    this.editDate = false;
+    this.iconDate = 'fa fa-calendar';
+    this.registerForm.patchValue({regDOB: this.date});
+  } else {
+    this.editDate = true;
+    this.iconDate = 'fa fa-check';
+  }
+}
 selectCar(event, overlaypanel: OverlayPanel) {
-  
-  overlaypanel.toggle(event);
+    overlaypanel.toggle(event);
 }
   selectInitialMenuItemBasedOnUrl() {
     let path = document.location.pathname;
