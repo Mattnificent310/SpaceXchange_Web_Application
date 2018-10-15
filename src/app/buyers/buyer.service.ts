4f3d3b2bdee9562326ec17fcf998c6c379ab3032
Matt Maree <dj_matt_maree@yahoo.com>
2018-10-15 18:20:07 -0400

    Update buyer.service.ts

--- src/app/buyers/buyer.service.ts
+++ src/app/buyers/buyer.service.ts
@@ -3,7 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Buyer} from './buyer.model';
 @Injectable()
 export class BuyerService {
-    webApiUrl = 'http://35.162.125.103:8083/users';
+    webApiUrl = 'http://http://18.203.81.222:8083/users';
     constructor(private http: HttpClient) {}
     getAllBuyers(): any {
         return this.http.get<Buyer[]>(this.webApiUrl);
