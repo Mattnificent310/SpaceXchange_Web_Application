c77a7bfff7335905732238067a6d834ad0e244fd
Matt Maree <dj_matt_maree@yahoo.com>
2019-01-18 20:38:19 +0200

    Converted images to webp

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -17,18 +17,18 @@ export class LandingComponent implements OnInit {
   ngOnInit() {
     this.myApp.hideMenu();
     this.images = [];
-    this.images.push({source: '../../assets/img/galleria1.jpg', alt: 'Description for Image 1', title:'Title 1'});
-    this.images.push({source: '../../assets/img/galleria2.jpg', alt: 'Description for Image 2', title:'Title 2'});
-    this.images.push({source: '../../assets/img/galleria3.jpg', alt: 'Description for Image 3', title:'Title 3'});
-    this.images.push({source: '../../assets/img/galleria4.jpg', alt: 'Description for Image 4', title:'Title 4'});
-    this.images.push({source: '../../assets/img/galleria5.jpg', alt: 'Description for Image 5', title:'Title 5'});
-    this.images.push({source: '../../assets/img/galleria6.jpg', alt: 'Description for Image 6', title:'Title 6'});
-    this.images.push({source: '../../assets/img/galleria7.jpg', alt: 'Description for Image 7', title:'Title 7'});
-    this.images.push({source: '../../assets/img/galleria8.jpg', alt: 'Description for Image 8', title:'Title 8'});
-    this.images.push({source: '../../assets/img/galleria9.jpg', alt: 'Description for Image 9', title:'Title 9'});
-    this.images.push({source: '../../assets/img/galleria10.jpg', alt: 'Description for Image 10', title:'Title 10'});
-    this.images.push({source: '../../assets/img/galleria11.jpg', alt: 'Description for Image 11', title:'Title 11'});
-    this.images.push({source: '../../assets/img/galleria12.jpg', alt: 'Description for Image 12', title:'Title 12'});
+    this.images.push({source: '../../assets/img/galleria1.webp', alt: 'Description for Image 1', title:'Title 1'});
+    this.images.push({ source: '../../assets/img/galleria2.webp', alt: 'Description for Image 2', title:'Title 2'});
+    this.images.push({ source: '../../assets/img/galleria3.webp', alt: 'Description for Image 3', title:'Title 3'});
+    this.images.push({ source: '../../assets/img/galleria4.webp', alt: 'Description for Image 4', title:'Title 4'});
+    this.images.push({ source: '../../assets/img/galleria5.webp', alt: 'Description for Image 5', title:'Title 5'});
+    this.images.push({ source: '../../assets/img/galleria6.webp', alt: 'Description for Image 6', title:'Title 6'});
+    this.images.push({ source: '../../assets/img/galleria7.webp', alt: 'Description for Image 7', title:'Title 7'});
+    this.images.push({ source: '../../assets/img/galleria8.webp', alt: 'Description for Image 8', title:'Title 8'});
+    this.images.push({ source: '../../assets/img/galleria9.webp', alt: 'Description for Image 9', title:'Title 9'});
+    this.images.push({ source: '../../assets/img/galleria10.webp', alt: 'Description for Image 10', title:'Title 10'});
+    this.images.push({ source: '../../assets/img/galleria11.webp', alt: 'Description for Image 11', title:'Title 11'});
+    this.images.push({ source: '../../assets/img/galleria12.webp', alt: 'Description for Image 12', title:'Title 12'});
 
   }
   scrollTop(event) {
