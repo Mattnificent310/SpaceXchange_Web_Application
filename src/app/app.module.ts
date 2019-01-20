393b0932f4a03f961a81a151c3189fa4ab218c71
Matt Maree <dj_matt_maree@yahoo.com>
2019-01-20 22:45:55 +0200

    Added Defer Load Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,6 +42,7 @@ import { NgxPageScrollModule } from 'ngx-page-scroll';
 import { CountoModule } from 'angular2-counto';
 import { UcWidgetModule } from "ngx-uploadcare-widget";
 import { InteractionsComponent } from './interactions/interactions.component';
+import { DeferLoadModule } from "@trademe/ng-defer-load";
 
 const appRoutes: Routes = [
   { path: "", redirectTo: "/landing", pathMatch: "full" },
@@ -131,7 +132,8 @@ const appRoutes: Routes = [
     CountoModule,
     FileUploadModule,
     UcWidgetModule,
-    SelectButtonModule
+    SelectButtonModule,
+    DeferLoadModule
   ],
   providers: [ConfirmationService, BuyerService, AppComponent],
   bootstrap: [AppComponent]
